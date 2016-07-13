af67b939f35b47ad5b2ea8e1b87d0c17f013903f
Ari Ruotsalainen <ari.ruotsalainen@solita.fi>
2016-07-13 10:21:58 +0300

    Cleanup xml processing in mock component. Tapio there is TODO!

--- src/test/java/fi/solita/mule/modules/xroad/LipaMockComponent.java
+++ src/test/java/fi/solita/mule/modules/xroad/LipaMockComponent.java
@@ -3,7 +3,6 @@
  */
 package fi.solita.mule.modules.xroad;
 
-import java.io.InputStream;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.util.Random;
@@ -17,7 +16,6 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
-import org.apache.cxf.helpers.IOUtils;
 import org.mule.api.MuleEventContext;
 import org.mule.api.MuleMessage;
 import org.mule.api.lifecycle.Callable;
@@ -26,7 +24,6 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.w3c.dom.Text;
 
 public class LipaMockComponent implements Callable {
 
@@ -39,37 +36,49 @@ public class LipaMockComponent implements Callable {
     }
 
     private MuleMessage handleResponse(MuleMessage message) throws Exception {
-        Object payloadObject = message.getPayload();
-        try (InputStream bis = (InputStream) payloadObject) {
-            byte[] bData = IOUtils.readBytesFromStream(bis);
-
-            Document requestDocument = getDocumentFromString(new String(bData, DEFAULT_ENCODING));
-            Node serviceNode = findNodeFromDocument(requestDocument, "helloService");
-            if (serviceNode == null) {
-                serviceNode = findNodeFromDocument(requestDocument, "getRandom");
-            }
-            serviceNode.appendChild(createResponseNode(requestDocument, serviceNode));
-            String response = getStringFromDocument(requestDocument);
-            message.setPayload(response);
-            return message;
-        }
+        Document requestDocument = getDocumentFromString(message.getPayloadAsString(DEFAULT_ENCODING));
+        Node bodyNode = findNodeFromDocument(requestDocument, "Body");
+        Node serviceNode = bodyNode.getChildNodes().item(0);
+        
+        // TODO: Tapio ei tämä synnytä oikean näköistä SOAP-responsea. Mikä tässä on ideana?
+        // Vastaukseksi tulee tyyliin <?xml version="1.0" encoding="UTF-8"?><ns2:getRandom xmlns:ns2="http://test.x-road.fi/producer"><request/><response><data>-549849087</data></response></ns2:getRandom>
+        serviceNode.appendChild(createResponseNode(requestDocument, serviceNode));
+        String response = getStringFromDocument(requestDocument);
+        message.setPayload(response);
+        return message;
+        
     }
 
     private Node createResponseNode(Document doc, Node serviceNode) {
         Element response = doc.createElement("response");
         Element message = null;
-        String operation = serviceNode.getNodeName();
-        if (operation.contains("helloService")) {
-            message = doc.createElement("message");
-            String callerName = serviceNode.getFirstChild().getFirstChild().getTextContent();
-            message.setTextContent("Hello: " + callerName + "!! Greetings from adapter server!");
-        } else if (operation.contains("getRandom")) {
-            message = doc.createElement("data");
-            message.setTextContent(Integer.toString(new Random(Runtime.getRuntime().freeMemory())
-                    .nextInt()));
+       
+        switch (serviceNode.getLocalName()) {
+        case "helloService":
+            message = createHelloServiceResponse(doc, serviceNode);
+            break;
+        case "getRandom":
+            message = createRandomServiceResponse(doc);
+            break;
+        default:
+            throw new IllegalArgumentException("Unknown operation " + serviceNode.getLocalName());
         }
         response.appendChild(message);
         return response;
+    }
+
+    private Element createRandomServiceResponse(Document doc) {
+        Element message = doc.createElement("data");
+        message.setTextContent(Integer.toString(new Random(Runtime.getRuntime().freeMemory())
+                .nextInt()));
+        return message;
+    }
+
+    private Element createHelloServiceResponse(Document doc, Node serviceNode) {
+        Element message = doc.createElement("message");
+        String callerName = serviceNode.getFirstChild().getFirstChild().getTextContent();
+        message.setTextContent("Hello: " + callerName + "!! Greetings from adapter server!");
+        return message;
     }
 
     public static Document getDocumentFromString(String content) throws Exception {
@@ -92,46 +101,32 @@ public class LipaMockComponent implements Callable {
         return sw.toString();
     }
 
-    public static Node findNodeFromDocument(Document doc, String expectedElementName) {
-        Element rootElement = doc.getDocumentElement();
-        NodeList nodeList = rootElement.getChildNodes();
-        for (int i = 0; i < nodeList.getLength(); i++) {
-            Node child = findNodeFromChildren(nodeList.item(i), expectedElementName);
-            if (child != null) {
-                return child;
+    public static Node findNodeFromDocument(Document doc, String nodeName) {
+        NodeList childNodes = doc.getChildNodes();
+        for (int i = 0; i < childNodes.getLength(); i++) {
+            Node node = findNodeFromNode(childNodes.item(i), nodeName);
+            if (node != null) {
+                return node;
             }
-
         }
         return null;
     }
-
-    public static Node findNodeFromChildren(Node node, String expectedElementName) {
-        if (node instanceof Text) {
+    
+    public static Node findNodeFromNode(Node node, String nodeName) {
+        if (nodeName.equals(node.getLocalName())) {
+            return node;
+        }
+            
+        NodeList childNodes = node.getChildNodes();
+        
+        if (childNodes.getLength() < 1) {
             return null;
         }
-        NodeList nodeList = node.getChildNodes();
-        if (nodeList == null || nodeList.getLength() < 1) {
-            return null;
-        }
-        for (int i = 0; i < nodeList.getLength(); i++) {
-            Node child = nodeList.item(i);
-            if (child instanceof Text) {
-                continue;
-            }
-            if (child != null && expectedElementName.equalsIgnoreCase(child.getLocalName())) {
-                return child;
-            } else if (child != null) {
-                NodeList childChildren = child.getChildNodes();
-                if (childChildren == null || childChildren.getLength() < 1) {
-                    continue;
-                }
-                for (int j = 0; j < childChildren.getLength(); j++) {
-                    Node responseChild = findNodeFromChildren(childChildren.item(j),
-                            expectedElementName);
-                    if (responseChild != null) {
-                        return responseChild;
-                    }
-                }
+        
+        for (int i = 0; i < childNodes.getLength(); i++) {
+            Node descendantNode = findNodeFromNode(childNodes.item(i), nodeName);
+            if (descendantNode != null) {
+                return descendantNode;
             }
         }
         return null;
